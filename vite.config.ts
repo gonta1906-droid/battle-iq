6615a337090e32d94fd0dc22d190c3d5c4ebaedb
Sergio <gonta1906@ukr.net>
2026-09-15 15:57:36 +0300

    Add Cloudflare deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [react()],
-
-  server: {
-    allowedHosts: [
-      "true-zebras-sleep.loca.lt",
-    ],
-  },
+  plugins: [react(), cloudflare()],
 });
